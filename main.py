eabc659d588fc2d96a0b96f089308ff082d489dd
NAIT BELKACEM <naitbelkacemyoussef@gmail.com>
2013-12-29 02:27:05 +0100

    Update main.py
    Ford-Fulkerson

--- main.py
+++ main.py
@@ -41,7 +41,31 @@ while num != 0:
 		for elt in res:
 			ordre_pieces += str(elt) + " "
 		print ordre_pieces + '\n' 
-
+	def ford():
+				
+		coupeMinimal=list()
+		dim=fulkerson_cours.getDimensions()
+		s=0
+		t=dim-1		
+		print "Matrice de test : "
+		print fulkerson_cours.affiche()
+		print "Matrice des Flux : " 
+		res = fulkerson_algo(fulkerson_cours,s,t,coupeMinimal)
+		print res.affiche()
+		source="S { "		
+		for elt in coupeMinimal:
+			source += str(elt) + " "
+		source+='}'
+		des="D { "
+		flotMaximal=0
+		for i in range(dim) :
+			if i not in coupeMinimal:
+				des+=str(i) + " "
+			flotMaximal+=res.getValue(0,i)
+		des+='}'
+		print "Le Flot maximal est : " + str(flotMaximal) + '\n'
+		print "La coupe minimale " + source + " " + des + '\n'
+		
 	def exit():
 		print "fin"
 	
@@ -50,8 +74,8 @@ while num != 0:
 		1 : dyn,
 		2 : floyd,
 		#3 : poten,
-		4 : johnson
-		#5 : ford,
+		4 : johnson,
+		5 : ford
 		#6 : branch,
 		#7 : simplexe
 		
